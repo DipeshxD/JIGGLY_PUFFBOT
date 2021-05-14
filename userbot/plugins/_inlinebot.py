7abad0aab39097c2f443673be4a4e34b23710c9b
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-14 15:37:08 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -148,7 +148,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     async def Information(event):
         if not event.query.user_id == bot.uid:
             return await event.answer(
-                "HELLO THERE. PLEASE MAKE YOUR OWN MAFIABOT AND USE. © MafiaBot ™",
+                "HELLO THERE. PLEASE MAKE YOUR OWN D3VILBOT AND USE. © D3vilBot ™",
                 cache_time=0,
                 alert=True,
             )
